9874c5e5b613eb696821d4a86cbf5a202e78db1b
sandeepmaity09 <sandeepmaity09@gmail.com>
2018-01-08 19:12:34 +0530

    token-auth

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secret':'thisissecret',
-    'database':'mongodb://root:root@localhost:27017/backenddb'
+    'database':'mongodb://localhost:27017/mydb'
 }
--- server.js
+++ server.js
@@ -12,16 +12,94 @@ var User = require('./app/models/user');
 var port = process.env.PORT || 8000;
 mongoose.connect(config.database);
 
-app.set('superSecret',config.secret);
+app.set('superSecret', config.secret);
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
-app.get('/',function(req,res){
-    res.send('Hello ! The API is at http://localhost:'+port+'/api');
+var apiRouter = express.Router();
+
+apiRouter.get('/', function (req, res) {
+    res.json({ message: 'Welcome to coolest API' })
+})
+
+apiRouter.post('/authenticate', function (req, res) {
+    User.findOne({
+        name: req.body.name
+    }, function (err, user) {
+        if (err)
+            throw err;
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed, User not found ' })
+        } else if (user) {
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed, Wrong Password' });
+            } else {
+                const payload = {
+                    admin: user.admin
+                }
+                var token = jwt.sign(payload, app.get('superSecret'), {
+                    expiresIn: 60
+                });
+
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                })
+            }
+        }
+    })
+});
+
+apiRouter.use(function (req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token' })
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        })
+    } else {
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        })
+    }
+})
+
+apiRouter.get('/users', function (req, res) {
+    User.find({}, function (err, users) {
+        res.json(users);
+    })
+})
+
+app.use('/api', apiRouter);
+
+app.get('/', function (req, res) {
+    res.send('Hello ! The API is at http://localhost:' + port + '/api');
+});
+
+app.get('/setup', function (req, res) {
+    // create a sample user
+    var nick = new User({
+        name: 'Nick Lodeon',
+        password: 'password',
+        admin: true
+    });
+
+    nick.save(function (err) {
+        if (err)
+            throw err;
+        console.log('User Saved Successfully');
+        res.json({ success: true })
+    })
 })
 
 app.listen(port);
-console.log('Server listening at http://localhost:'+port);
+console.log('Server listening at http://localhost:' + port);
